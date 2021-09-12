3ebad87db52fa744877fc8062ba81350f11ecf0b
kchomg <kchomg@naver.com>
2021-09-12 22:11:29 +0900

    second commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Seok Eun Hong on 2021/09/12.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let description: String
+    let id: Int
+}
